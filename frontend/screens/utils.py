31b46623ff6447acbf61b0821a4f115c08422d62
cristiansaracino <info.cristiansaracino@gmail.com>
2026-07-05 18:25:55 +0200

    pulsante conferma uscita

--- frontend/screens/utils.py
+++ frontend/screens/utils.py
@@ -2,10 +2,8 @@ import pygame
 import sys
 
 def _handle_quit(ev):
-
-    if ev.type == pygame.QUIT or (
-        ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE
-    ):
-
+    # Lasciamo solo l'evento QUIT (la X della finestra o il post() che abbiamo appena creato).
+    # La gestione del tasto ESC è ora delegata alle singole schermate (come il tuo form).
+    if ev.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
